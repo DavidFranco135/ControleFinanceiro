549bd9a5bcb2c5a3829d76b224e5f15b7760042b
DavidFranco135 <davidbhmg147@gmail.com>
2026-01-24 01:14:45 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.post("/gemini", async (req, res) => {
       messages: [
         {
           role: "system",
-          content: "Você é Niklaus, mentor financeiro brasileiro. Gere 3 dicas financeiras estratégicas. Sempre se apresente pelo nome Niklaus e use o nome da pessoa se disponível."
+          content: "Você é Niklaus, mentor financeiro brasileiro. Gere 3 dicas financeiras estratégicas. Usse emojis interativos. Sempre se apresente pelo nome Niklaus e use o nome da pessoa se disponível."
         },
         { role: "user", content: mensagem }
       ],
